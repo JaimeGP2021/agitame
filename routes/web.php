5cd1bd3750fdffafc79d48d13dc87adce29b32d8
Jaime García <jaime.garcia@iesdonana.org>
2024-11-11 20:29:54 +0100

    Konami code added as route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/konami', function () {
+    return 'Up, Up, Down, Down, Left, Right, Left, Right, B, A';
+});
